11be07f66fc8cd2a3415130e81fd0515cdebd894
dowenwow <dowenwow@gmail.com>
2025-12-08 14:47:52 +0100

    Initial commit.

---
+++ src/main/java/com/redcare/RedcareTestApplication.java
@@ -0,0 +1,13 @@
+package com.redcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedcareTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedcareTestApplication.class, args);
+	}
+
+}
